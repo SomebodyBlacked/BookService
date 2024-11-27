b817f431d19f3058390835e890e5d0a97b9058ba
Alguien <WhiteComment@protonmail.com>
2024-11-27 23:52:59 +0000

    add deleteBook method

--- src/main/java/com/sb/demo/controllers/BooksController.java
+++ src/main/java/com/sb/demo/controllers/BooksController.java
@@ -3,6 +3,7 @@ package com.sb.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,10 @@ public class BooksController {
   @PostMapping
   public Book addBook(@RequestBody Book book) {
     return bookRepository.save(book);
+  }
+
+  @DeleteMapping
+  public void deleteBook(@RequestBody long id) {
+    bookRepository.deleteById(id);
   }
 }
